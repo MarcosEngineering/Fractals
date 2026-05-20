8df1ada2b9e7ec7e1bc1479bacd3d2eb57b8397c
MarcosEngineering <mcampolo70@gmail.com>
2026-05-20 05:46:25 +0200

    Add files via upload

--- Roger_Fractal.py
+++ Roger_Fractal.py
@@ -1,9 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 20 05:26:39 2026
+Created on Sat May 16 14:21:15 2026
 
-@author: mcamp
+
+Copyright (c) 2026, Campolo Marco
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
 """
+
+#----------------------------------------
+# Create a Roger's Fractal
+# 
+#----------------------------------------
+# Author : Marco Campolo 2026/05/20
 
 import numpy as np
 import matplotlib.pyplot as plt
